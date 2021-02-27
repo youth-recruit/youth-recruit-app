4c519bf7cbf419e439fd4093813beda09649026b
jainamdoshi <jainam.doshi2001@gmail.com>
2021-02-27 17:44:49 +0530

    add firebase with auth cli

--- youth-recruit/firebase.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBqKcJPFtCdKMN5t1kJoq3ZW9Xt67fA6nM",
-    authDomain: "youth-recruit-development.firebaseapp.com",
-    projectId: "youth-recruit-development",
-    storageBucket: "youth-recruit-development.appspot.com",
-    messagingSenderId: "996433782658",
-    appId: "1:996433782658:web:72cdb22c535c2b6795919b",
-    measurementId: "G-F18R4ZSJEB"
-};
---
+++ youth-recruit/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyB8Dd07ft7lBhQy7SdY7eLF0M_r80u8hLI",
+    authDomain: "youth-recruit-2bb4d.firebaseapp.com",
+    projectId: "youth-recruit-2bb4d",
+    storageBucket: "youth-recruit-2bb4d.appspot.com",
+    messagingSenderId: "669816392746",
+    appId: "1:669816392746:web:d1e228368869ebd9350753",
+    measurementId: "G-NL7T7JCZWZ"
+});
+
+export default app;
+export const auth = app.auth;
