a93b3df810da9a587c760a2445fd51827099b021
jam-dos <s3825891@student.rmit.edu.au>
2021-03-17 12:47:00 +0400

    Add searchBar component file to the JobPosting

--- youth-recruit/src/Components/home/JobPosting.js
+++ youth-recruit/src/Components/home/JobPosting.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./jobPosting.css"
+import SearchBar from './SearchBar'
 
 export default function JobPosting() {
     return (
         <div>
-            
+            <SearchBar />
         </div>
     )
 }
